5e6b74bc9d405a4c928c4ba695aadb87a5e883e2
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-09-13 15:48:11 +0100

    Added test for prices

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -2,10 +2,14 @@ require 'food'
 
 describe Food do
 
-let(:korma) {food.new(Korma)}
+let(:korma) {Food.new(Korma)}
 
 	it 'should have a name' do
 		food.name == Korma
+	end
+
+	it 'should contain a price for each food item' do
+		food.items[korma].should eq 4.95
 	end
 
 end
